2821b2de1e0aa04e1af2c42bc017eca84d9ad8de
Arif Shaikh <shaikharif006@gmail.com>
2024-09-10 19:24:11 +0530

    code refactor

---
+++ src/main/java/com/example/makemytrip/flights.java
@@ -0,0 +1,11 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flights {
+
+    @GetMapping("/flights")
+    public String getData() {return "Please book your flight at 10% discount from Indigo" ; }
+}
---
+++ src/main/java/com/example/makemytrip/train.java
@@ -0,0 +1,11 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class train {
+
+    @GetMapping("/train")
+    public String getData() {return "Please book your train at 10% discount in Vande Bharat Express" ; }
+}
